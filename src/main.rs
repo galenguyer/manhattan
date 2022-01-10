515dc9148c9d5df82a3445ae994615632f96a099
Galen Guyer <galen@galenguyer.com>
2022-01-10 18:35:05 -0500

    add dotenv and keycloak_token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use dotenv;
+use std::env;
+
 fn main() {
+    dotenv::dotenv().ok();
+    let _keycloak_token = env::var("KEYCLOAK_TOKEN").expect("KEYCLOAK_TOKEN not set");
+
     println!("Hello, world!");
 }
