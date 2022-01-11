da2de84c3ed427f249019c0e310cb1b83b7001c9
Galen Guyer <galen@galenguyer.com>
2022-01-10 19:16:54 -0500

    get drink url from env

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ use std::env;
 fn main() {
     dotenv::dotenv().ok();
     let keycloak_token = env::var("KEYCLOAK_TOKEN").expect("KEYCLOAK_TOKEN not set");
+    let drink_url = env::var("DRINK_URL").unwrap_or("https://drink.csh.rit.edu/drinks".to_string());
 
     let client = Client::new();
-    let drink_response = client.get("https://drink.csh.rit.edu/drinks").bearer_auth(keycloak_token).send().unwrap();
+    let drink_response = client.get(drink_url).bearer_auth(keycloak_token).send().unwrap();
 
     if drink_response.status().is_success() {
+        let response_text = drink_response.text().unwrap();
+        println!("{}", response_text);
     } else {
         let response_status = drink_response.status();
         let response_text = drink_response.text().unwrap();
