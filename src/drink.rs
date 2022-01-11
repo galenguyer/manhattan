fe264281c80f76f12d7e1b9d8d33116d837c0a37
Galen Guyer <galen@galenguyer.com>
2022-01-10 19:35:02 -0500

    fetch data and deserialize it properly

---
+++ src/drink.rs
@@ -0,0 +1,30 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Response {
+    pub machines: Vec<Machine>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Machine {
+    pub display_name: String,
+    pub id: u32,
+    pub is_online: bool,
+    pub name: String,
+    pub slots: Vec<Slot>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Slot {
+    pub number: u32,
+    pub active: bool,
+    pub empty: bool,
+    pub item: Item
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Item {
+    pub id: u32,
+    pub name: String,
+    pub price: u32,
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use dotenv;
 use reqwest::blocking::Client;
 use serde_json::Value;
 use std::env;
+mod drink;
 
 fn main() {
     dotenv::dotenv().ok();
@@ -9,18 +10,24 @@ fn main() {
     let drink_url = env::var("DRINK_URL").unwrap_or("https://drink.csh.rit.edu/drinks".to_string());
 
     let client = Client::new();
-    let drink_response = client.get(drink_url).bearer_auth(keycloak_token).send().unwrap();
+    loop {
+        let drink_response = client.get(&drink_url).bearer_auth(&keycloak_token).send().unwrap();
 
-    if drink_response.status().is_success() {
-        let response_text = drink_response.text().unwrap();
-        println!("{}", response_text);
-    } else {
-        let response_status = drink_response.status();
-        let response_text = drink_response.text().unwrap();
-        let error = match serde_json::from_str::<Value>(&response_text) {
-            Ok(v) => v["error"].as_str().unwrap().to_owned(),
-            Err(_) => "error deserializing response".to_owned(),
-        };
-        println!("Whoops, drink returned a {} ({})", response_status, error);
+        if drink_response.status().is_success() {
+            let response_text = drink_response.text().unwrap();
+            let response: drink::Response = serde_json::from_str(&response_text).unwrap();
+            let machines = response.machines;
+            println!("{:?}", machines);
+        } else {
+            let response_status = drink_response.status();
+            let response_text = drink_response.text().unwrap();
+            let error = match serde_json::from_str::<Value>(&response_text) {
+                Ok(v) => v["error"].as_str().unwrap().to_owned(),
+                Err(_) => "error deserializing response".to_owned(),
+            };
+            println!("Whoops, drink returned a {} ({})", response_status, error);
+        } 
+        
+        std::thread::sleep(std::time::Duration::from_secs(5))
     }
 }
